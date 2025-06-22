ea23995518e3ffde9a0284ac463bd9931b4b8ea0
nehalearnsGit <msk.neha@gmail.com>
2025-06-22 14:26:23 +0530

    Add files via upload
    day1

---
+++ day1/Firefox.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String browserName = "Firefox";
+		float browserVersion = 100.2f;
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		
+		System.out.println(browserName);
+		System.out.println(browserVersion);
+		System.out.println(isVisible);
+		System.out.println(releaseYear);
+		System.out.println(browserLogo);
+		
+	}
+
+}
+
